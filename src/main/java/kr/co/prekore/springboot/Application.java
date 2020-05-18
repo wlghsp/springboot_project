113a9321e14754fef7be124e7768113bfb3fdb95
jihochoi <wlghsp@gmail.com>
2020-05-18 19:11:28 +0900

    등록/수정/조회 API JPA Auditing chapter4

--- src/main/java/kr/co/prekore/springboot/Application.java
+++ src/main/java/kr/co/prekore/springboot/Application.java
@@ -2,8 +2,9 @@ package kr.co.prekore.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing      // Jpa Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
